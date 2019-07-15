e13792bf4d8c4699cc36641136c2a9ecab06694f
FanYaFei180 <2324896104@qq.com>
2019-07-15 09:27:26 +0800

    ‘备fanyafei修改‘

---
+++ src/page/Main_branch/invoice_management.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="invoice">
+    <!-- 头部 -->
+    <Branch :title="title">
+      <router-link to="/" class="leftImg" slot="left">
+        <img src="./img/fanhui@2x.png" alt />
+      </router-link>
+    </Branch>
+    <!-- 内容 -->
+    <router-link to="/make_invoice">
+      <Setinvoce :arr="arr[0]"></Setinvoce>
+    </router-link>
+    <router-link to="/billing_history">
+      <Setinvoce :arr="arr[1]"></Setinvoce>
+    </router-link>
+    <router-link to="/common_information">
+      <Setinvoce :arr="arr[2]" style="border:none"></Setinvoce>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Setinvoce from "./set_invoce";
+import Branch from "./branch_head";
+export default {
+  data() {
+    return {
+      title: "发票管理",
+      arr: ["根据订单开票", "开票历史记录", "常用信息设置"]
+    };
+  },
+  methods: {},
+  components: {
+    Setinvoce,Branch
+  }
+};
+</script>
+
+<style scoped lang='less'>
+.invoice {
+  font-size: 0.13rem;
+  color: #444;
+  height: 100%;
+  background: #f6f6f6;
+}
+</style>
---
+++ src/page/regulations/two_regua/other_processing.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- 其他处理效果 -->
+  <div class="other_processing">
+ <Nthings :p="p"></Nthings>
+  </div>
+</template>
+
+<script>
+import Nthings from './../../Main_branch/nothings'
+export default {
+  data() {
+    return {
+ p:'当前页面没有相关数据哦'
+    }
+  },
+  methods: {
+
+  },
+  components: {
+Nthings
+  }
+}
+</script>
+
+<style scoped lang='less'>
+
+</style>
